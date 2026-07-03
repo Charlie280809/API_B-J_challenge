eb2d5453fc808e8a88e4e62634fb627451b7ddc6
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-07-03 20:38:51 +0200

    feat: initialize Express server and connect to MongoDB

---
+++ index.js
@@ -0,0 +1,40 @@
+createRequire("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoUrl = process.env.MONGO_URL;
+if(!mongoUrl) {
+    console.error("MONGO_URL is not defined in the environment variables.");
+    process.exit(1);
+}
+mongoose.connect(mongoUrl)
+    .then(() => console.log(`Connected to ${mongoUrl}`))
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+        process.exit(1);
+    });
+
+    async function start() {
+        try {
+            await mongoose.connect(mongoUrl);
+            console.log("Connected to MongoDB");
+
+            app.listen(port, () => {
+                console.log(`Server is running on port ${port}`);
+            });
+        } catch (err) {
+            console.error("Error connecting to MongoDB:", err);
+            process.exit(1);
+        }
+    }
+    start();

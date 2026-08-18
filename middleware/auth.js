b15bdb393cd83d45160eca2f41d6ec01f099d26f
Charlie Van Belle <r0995521@student.thomasmore.be>
2026-08-18 17:58:50 +0200

    fix: refactor authentication middleware to export directly

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 
-const authenticateToken = (req, res, next) => {
+module.exports = (req, res, next) => {
     const token = req.headers.authorization?.split(' ')[1]
     if (!token) return res.status(401).json({ message: 'No token' })
 
@@ -11,5 +11,3 @@ const authenticateToken = (req, res, next) => {
         res.status(403).json({ message: 'Invalid token' })
     }
 }
-
-module.exports = { authenticateToken }
